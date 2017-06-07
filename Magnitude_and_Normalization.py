0c5bc8203d350a12398b771deecab14006fce0fb
John Chang <johnchang9094@gmail.com>
2017-06-07 22:11:08 +0100

    Finding Magnitude

--- Magnitude_and_Normalization.py
+++ Magnitude_and_Normalization.py
@@ -43,4 +43,18 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
+
+v = Vector([-0.221, 7.437])
+print (v.magnitude())
+
+v = Vector([8.813, -1.331, -6.247])
+print (v.magnitude())
+
+# v = Vector([5.581, -2.136])
+# print (v.normalized())
+
+# v = Vector([1.996, 3.108, -4.554])
+# print (v.normalized())
+
+
         
